1ebab1cd902812ed73175160e30dc83f442bbb5e
Sindre L. Øyen <sindroye@stud.ntnu.no>
2024-11-20 11:29:09 +0100

    feat: create counting script

---
+++ aggregations/count_entries.py
@@ -0,0 +1,22 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+import constants
+import json
+
+lake_data_path = constants.AggregateData.aggregated_lake_data
+river_data_path = constants.AggregateData.aggregated_river_data
+
+# Count entries in the lake data
+with open(lake_data_path, "r", encoding="utf-8") as file:
+    lake_data = json.load(file)
+    lake_count = sum(len(region_data) for region_data in lake_data.values())
+
+# Count entries in the river data
+with open(river_data_path, "r", encoding="utf-8") as file:
+    river_data = json.load(file)
+    river_count = sum(len(region_data) for region_data in river_data.values())
+
+print(f"Total entries in lake data: {lake_count}")
+
+print(f"Total entries in river data: {river_count}")
